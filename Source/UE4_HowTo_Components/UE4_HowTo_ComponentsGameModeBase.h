a070e4501a403bbac9ae06c9e6d24038500b9ab0
Adam Spanik <adam.spanik@seznam.cz>
2022-07-05 17:38:07 +0200

    init

---
+++ Source/UE4_HowTo_Components/UE4_HowTo_ComponentsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_HowTo_ComponentsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_HOWTO_COMPONENTS_API AUE4_HowTo_ComponentsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
