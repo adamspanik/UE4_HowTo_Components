6b51f18ae91be7494dd6f52c470bebf9ae3dae27
Adam Spanik <adam.spanik@seznam.cz>
2022-07-05 18:47:48 +0200

    1. Creating and Attaching Components

---
+++ Source/UE4_HowTo_Components/CollidingPawn.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "CollidingPawn.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+
+
+// Sets default values
+ACollidingPawn::ACollidingPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Our root component will be a sphere that reacts to physics
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->InitSphereRadius(40);
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
+
+	// Create and position a mesh component so we can see where our sphere is
+	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
+	SphereVisual->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (SphereVisualAsset.Succeeded())
+	{
+		SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+		SphereVisual->SetRelativeLocation(FVector(0,0,-40));
+		SphereVisual->SetWorldScale3D(FVector(0.8));
+	}
+
+	// Create a particle system that we can activate or deactivate
+	OurParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovementParticles"));
+	OurParticleSystem->SetupAttachment(SphereVisual);
+	OurParticleSystem->bAutoActivate = false;
+	OurParticleSystem->SetRelativeLocation(FVector(-20,	0, 20));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	if (ParticleAsset.Succeeded())
+	{
+		OurParticleSystem->SetTemplate(ParticleAsset.Object);
+	}
+
+
+	// Use a spring arm to give the camera smooth, natural-feeling motion.
+	USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraAttachmentArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->SetRelativeRotation(FRotator(-45,0,0));
+	SpringArm->TargetArmLength = 400;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 3;
+
+	// Create a camera and attach to our spring arm
+	UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("ActualCamera"));
+
+	// Take control of the default player
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	
+	
+}
+
+// Called when the game starts or when spawned
+void ACollidingPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollidingPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACollidingPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
